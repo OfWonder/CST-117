9b640c3fcbe4d9aa0473948fb657223f18695226
OfWonder <47732866+OfWonder@users.noreply.github.com>
2019-08-11 17:33:39 -0700

    Exercise 1

---
+++ Week 1/Exercise 1/CST-117_Exercise_1/CST-117_Exercise_1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_117_Exercise_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "You Clicked Label 1";
+            updateList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+     
+                label1.Text = "You pressed button 1 AND checkbox 1 is checked";
+            else
+                label1.Text = "You pressed button 1";
+
+            updateList();
+        }
+
+        private void updateList()
+        {
+            listBox1.Items.Add(label1.Text);
+            listBox1.Update();
+        }
+    }
+}
