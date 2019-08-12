9b34e38f7f6abb640b6e326e4692aef87b68df17
OfWonder <47732866+OfWonder@users.noreply.github.com>
2019-08-11 21:14:28 -0700

    Programming Project 3

---
+++ Week 1/Programming Project 1/CST-117_Programming_Project_1/CST-117_Programming_Project_1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_117_Exercise_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Visible = false;
+            textBox2.Visible = false;
+            label2.Visible = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                button1.Visible = false;
+            }
+            else
+            {
+                button1.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            try
+            {
+                decimal input = Decimal.Parse(textBox1.Text);
+                textBox2.Text = (input + 5).ToString("#.000");
+                textBox2.Visible = true;
+                label2.Visible = true;
+                button1.Text = "Re-Calculate!";
+            }
+            catch(FormatException)
+            {
+                textBox2.Visible = true;
+                textBox2.Text = "Please enter a number.";
+            }
+        }
+    }
+}
